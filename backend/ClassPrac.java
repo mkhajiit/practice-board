99b66028248ddf045535acd0b626da0f383c8494
mkhajiit <mkhajiit15@gmail.com>
2025-02-20 18:03:00 +0900

    주석 추가

--- backend/ClassPrac.java
+++ backend/ClassPrac.java
@@ -8,6 +8,7 @@ public class ClassPrac {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
+        // 클래스 호출
         for_import.Number number = new Number();
 
         number.generateRandomNumber();
@@ -20,8 +21,10 @@ public class ClassPrac {
         System.out.println("본인의 이름을 입력해주세요: ");
         String name = scanner.nextLine();
 
+        // 클래스 호출 생성자 실행
         Person person = new Person(name);
 
+        // Person 클래스의 setter로 나이를 설정함;
         person.setAge(age);
 
         person.introduce();
