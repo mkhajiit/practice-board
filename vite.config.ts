a5128d3ee2550016b25e509f88c75b65eac99c6d
mkhajiit <mkhajiit15@gmail.com>
2025-02-22 20:17:40 +0900

    이미지 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
+// 포트번호 설정
 export default defineConfig({
   server: {
     port: 3000,
