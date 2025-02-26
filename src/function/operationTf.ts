71b68cea56e4dda92f9a50cdf83fae62c5778f8a
mkhajiit <mkhajiit15@gmail.com>
2025-02-27 02:02:52 +0900

    데이터셋 자동화 함수 생성

--- src/function/operationTf.ts
+++ src/function/operationTf.ts
@@ -1,14 +1,17 @@
+// 반드시 리펙토링해보기
 import * as tf from '@tensorflow/tfjs';
-import { xs, ys } from '../constants/operationData';
+import { generateDataset } from '../constants/operationData';
 
 // 모델 저장 경로
 const MODEL_PATH = 'localstorage://operation-model';
-
+// 데이터셋 400개
+const dataset = generateDataset(600);
+const xs = dataset.xs;
+const ys = dataset.ys;
 // 모델 생성 함수
 const createModel = () => {
   const model = tf.sequential();
-  model.add(tf.layers.dense({ units: 32, inputShape: [3], activation: 'relu' }));
-  model.add(tf.layers.dense({ units: 16, activation: 'relu' }));
+  model.add(tf.layers.dense({ units: 16, inputShape: [3], activation: 'relu' }));
   model.add(tf.layers.dense({ units: 8, activation: 'relu' }));
   model.add(tf.layers.dense({ units: 1 })); // 출력은 1개
 
@@ -22,7 +25,7 @@ const createModel = () => {
 
 // 모델 학습 함수
 const trainModel = async (model: tf.Sequential) => {
-  await model.fit(xs, ys, { epochs: 10000 });
+  await model.fit(xs, ys, { epochs: 2000, batchSize: 16, validationSplit: 0.2 });
   await model.save(MODEL_PATH);
   console.log('Model trained and saved.');
 };
