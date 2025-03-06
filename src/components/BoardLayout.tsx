6a12afd7f08deab984c299a847110de765f99c6f
mkhajiit <mkhajiit15@gmail.com>
2025-03-06 22:48:21 +0900

    boardlayout에 미디어 쿼리 추가

--- src/components/BoardLayout.tsx
+++ src/components/BoardLayout.tsx
@@ -5,15 +5,23 @@ import Navigation from './Navigation';
 
 // 스타일드 컴포넌트
 const Layout = styled.div`
-  width: 80%;
-  height: 60%;
-  min-width: 500px;
-  min-height: 400px;
-  border: 5px solid #b33939;
+  width: 100%;
+  height: auto;
+  min-width: 320px;
+  min-height: 100vh;
+  border: none;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  @media (min-width: 768px) {
+    width: 80%;
+    height: 60%;
+    min-width: 500px;
+    min-height: 400px;
+    border: solid #b33939 7px;
+  }
 `;
 
 function BoardLayout() {
